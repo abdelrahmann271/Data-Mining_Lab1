7a05316cbc96d92de1d5b6fb0c6d006caea0be8e
Abdelrahman Nour <abdelrahmann271@gmail.com>
2021-10-24 15:39:03 +0200

    adding some plots

--- Lab1.py
+++ Lab1.py
@@ -23,16 +23,29 @@ sns.countplot(x=1,data=df)
 
 #For some features
 
-#plt.figure()
-#sns.histplot(data=df, x=12,hue=1)
-#plt.figure()
-#sns.histplot(data=df, x=22,hue=1)
-
-
-#4 Scatter Plot
 plt.figure()
+sns.histplot(data=df, x=12,hue=1)
+plt.figure()
+sns.histplot(data=df, x=22,hue=1)
+
+
+#4 Scatter Plot for mean features.
+
 g = df[[i for i in range(2,12)]]
 sns.pairplot(g)
+
+# Scatter Plot for SE features.
+
+g = df[[i for i in range(12,22)]]
+sns.pairplot(g)
+
+# Scatter Plot for worst features.
+
+g = df[[i for i in range(22,32)]]
+sns.pairplot(g)
+
+
+
 
 #5 Correlation matrix
 plt.figure()
@@ -46,12 +59,20 @@ column = [i for i in range(2,32)]
 df[column] = (df[column] - df[column].mean()) / df[column].std()
 print(df)
 
+#Box plot for all features..
+#Individually ..
 # for i in range(2, 31):
 #     plt.figure()
 #     sns.boxplot(x=1, y=i , data=df)
 
-plt.figure()
-sns.boxplot(x=1, y=2 , data=df )
+
+fig, axes = plt.subplots(5,6, figsize=(20, 20))
+cnt=2
+for i in range(0, 5):
+    for j in range(0, 6):
+        sns.boxplot(ax=axes[i, j],x=1, y=cnt, data=df )
+        axes[i][j].set_title(cnt)
+        cnt = cnt + 1
 
 
 plt.show()
